367e3fe35c55f3629b3c20ae3ac3e00b303001f7
Sergio Mir <voidtracer6@gmail.com>
2023-07-04 22:49:04 -0400

    ./scr/components/shared/Footer: added the elements of the footer | ./scr/components/shared: in all anchors changed the effect when active

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,7 +1,22 @@
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 left-0 w-full text-white bg-[#202026] py-20'>
-      <article className=''></article>
+    <footer className='absolute bottom-0 left-0 w-full text-white bg-[#202026] py-10'>
+      <div className="w-full flex justify-center">
+        <ul className="flex gap-10">
+          <li>
+            <a href="#" className="font-belanosima rounded-lg py-1 px-2 hover:bg-slate-600 transition duration-150 active:bg-slate-700">Terms</a>
+            </li>
+          <li>
+            <a href="#" className="font-belanosima rounded-lg py-1 px-2 hover:bg-slate-600 transition duration-150 active:bg-slate-700">Privacy</a>
+            </li>
+          <li>
+            <a href="#" className="font-belanosima rounded-lg py-1 px-2 hover:bg-slate-600 transition duration-150 active:bg-slate-700">Contact</a>
+            </li>
+          <li>
+            <a href="#" className="font-belanosima rounded-lg py-1 px-2 hover:bg-slate-600 transition duration-150 active:bg-slate-700">Copyright</a>
+            </li>
+        </ul>
+      </div>
     </footer>
   )
 }
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -2,13 +2,13 @@ const Header = () => {
   return (
     <header className='font-belanosima text-xl text-white'>
       <ul className='flex justify-end bg-[#202026] gap-2 py-4 pr-4'>
-        <li className='hover:bg-slate-700 hover:transition duration-150 rounded-lg'>
+        <li className='hover:bg-slate-600 hover:transition duration-150 active:bg-slate-700 rounded-lg'>
           <a href="#" className='py-1 px-2 '>Home</a>
         </li>
-        <li className='group hover:bg-slate-700 hover:transition duration-150 rounded-lg'>
+        <li className='group hover:bg-slate-600 hover:transition duration-150 active:bg-slate-700 rounded-lg'>
           <a href="#" className='py-1 px-2'>To Do</a>
         </li>
-        <li className='hover:bg-slate-700 hover:transition duration-150 rounded-lg'>
+        <li className='hover:bg-slate-600 hover:transition duration-150 active:bg-slate-700 rounded-lg'>
           <a href="#" className='py-1 px-2 '>Profile</a>
         </li>
       </ul>
