607fcfe4b35e204ce5c9255b71a5f31f5daaafe1
Sergio Mir <voidtracer6@gmail.com>
2023-07-05 18:36:47 -0400

    general improvements on accesibility

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,17 +6,17 @@ const Home = () => {
         <img src="./images/6926528.webp" width="384" height="384" alt="An images that resembles the process of organizing a project" className="max-h-96 shadow-2xl shadow-slate-900 translate-x-24 rounded-xl z-10 animate-fade-right animate-once animate-duration-[1200ms]" />
         <ul className="-translate-y-6 -translate-x-36">
           <li className="pl-80 bg-slate-600 p-2 rounded-lg pt-6 pb-2 mb-3 text-center animate-fade-left animate-once animate-duration-[1200ms] animate-delay-200">
-            <p className="font-belanosima text-xl text-slate-300 -translate-y-2 -translate-x-32">
+            <p className="font-belanosima text-xl text-white -translate-y-2 -translate-x-32">
               This is a <span className="text-sky-500">To-Do app</span>. Here you can register the <br /> tasks that you have pending to do and mark  their <br /> completion once they are done.
             </p>
           </li>
           <li className="pl-80 bg-slate-600 py-4 rounded-lg mb-3 text-center animate-fade-left animate-once animate-duration-[1200ms] animate-delay-300">
-            <p className="font-belanosima text-xl text-slate-300 -translate-x-32">
+            <p className="font-belanosima text-xl text-white -translate-x-32">
               <span className="text-sky-500">Don't let your to-do list overwhelm you</span>. Our app <br /> helps you break down your tasks into manageable   <br /><span className="text-sky-500">steps</span>, so you can tackle them one at a time <br /> and feel a sense of accomplishment along the   way.
             </p>
           </li>
           <li className="pl-80 bg-slate-600 pt-2 pb-7 rounded-lg text-center animate-fade-left animate-once animate-duration-[1200ms] animate-delay-500">
-            <p className="font-belanosima text-xl text-slate-300 -translate-x-32 translate-y-2">
+            <p className="font-belanosima text-xl text-white -translate-x-32 translate-y-2">
               <span className="text-sky-500">Simplify your life</span> with our to-do app. Say goodbye <br /> to cluttered notebooks and sticky notes - our app <br /> gives you a clean and streamlined way to manage <br /> your tasks and <span className="text-sky-500">stay organized</span>.
             </p>
           </li>
