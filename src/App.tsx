eaca83162ed679e40696b1a5aab036d2ef479375
Serio_Mir <voidtracer6@gmail.com>
2023-07-23 23:20:35 -0400

    ./src/App.tsx: MOD
    changed the way the routing behaves and now in the Sing up and Register the Header and Footer elements will hide

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,57 @@
-import Header from './components/shared/Header';
+import { useEffect, useState } from "react";
+import Header from "./components/shared/Header";
 import Footer from "./components/shared/Footer";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Register from './components/Register/Register';
-import SingIn from './components/Register/SingIn';
-import Profile from './components/Register/Profile';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import SingIn from "./components/Register/SingIn";
+import Profile from "./components/Register/Profile";
 
 function App() {
-
-  const routes = [
-    { path: '/ToDo-web', element: <Home /> },
-    { path: '/ToDo-web/register', element: <Register /> },
-    { path: '/ToDo-web/sing-in', element: <SingIn /> },
-    { path: '/ToDo-web/profile', element: <Profile /> }
-  ];
-
   return (
     <>
       <main className="relative">
         <Router>
           <div className="min-h-screen overflow-y-hidden">
-            <Header />
             <Routes>
-              {routes.map(({ path, element }) => (
-                <Route key={path} path={path} element={element} />
-              ))}
+              <Route path="/ToDo-web/*" element={<HeaderFooterController />}>
+                <Route index element={<Home />} />
+                <Route path="register" element={<Register />} />
+                <Route path="sing-in" element={<SingIn />} />
+                <Route path="profile" element={<Profile />} />
+              </Route>
             </Routes>
-            <Footer />
           </div>
         </Router>
       </main>
     </>
-  )
+  );
+
+  function HeaderFooterController() {
+    const location = useLocation();
+    const [hideHeaderFooter, setHideHeaderFooter] = useState(false);
+
+    useEffect(() => {
+      const isRegisterOrSignInRoute =
+        location.pathname.includes("/register") ||
+        location.pathname.includes("/sing-in");
+      setHideHeaderFooter(isRegisterOrSignInRoute);
+    }, [location]);
+
+    return (
+      <>
+        {!hideHeaderFooter && <Header />}
+        <Outlet />
+        {!hideHeaderFooter && <Footer />}
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
