bab749c714482ce9807bb385d5685497facfda9a
Serio_Mir <voidtracer6@gmail.com>
2023-07-24 11:03:52 -0400

    MOD
    ./scr/index.css: MOD
    
    the style of the inputs related to the sing up and the log in has been extracted to the global css for a better code lecture

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -4,31 +4,25 @@ export default function Register() {
   return (
     <section className="text-white flex items-center flex-col mt-32">
       <h1 className="text-5xl font-belanosima-semibold">Register</h1>
-      <input
-        type="text"
-        className="mt-4 bg-transparent w-[16rem] p-2 border-2 border-dashed border-slate-600 rounded-md outline-none focus:border-white focus:border-solid  transition duration-150"
-        placeholder="name..."
-      />
-      <input
-        type="email"
-        className="mt-4 bg-transparent w-[16rem] p-2 border-2 border-dashed border-slate-600 rounded-md outline-none focus:border-white focus:border-solid  transition duration-150"
-        placeholder="email..."
-      />
+      <input type="text" 
+      className="register-inputs" 
+      placeholder="name..." />
+      <input 
+      type="email" 
+      className="register-inputs" 
+      placeholder="email..." />
       <input
         type="password"
-        className="mt-4 bg-transparent w-[16rem] p-2 border-2 border-dashed border-slate-600 rounded-md outline-none focus:border-white focus:border-solid  transition duration-150"
-        placeholder="password..."
-      />
+        className="register-inputs"
+        placeholder="password..." />
       <input
         type="password"
-        className="mt-4 bg-transparent w-[16rem] p-2 border-2 border-dashed border-slate-600 rounded-md outline-none focus:border-white focus:border-solid  transition duration-150"
-        placeholder="confirm password..."
-      />
+        className="register-inputs"
+        placeholder="confirm password..." />
       <div className="mt-4 py-1 px-3 cursor-pointer select-none rounded-lg border-2 group active:border-purple-500 active:bg-gray-700 hover:bg-slate-50 transition duration-150">
         <Link
           to="/ToDo-web/sing-in"
-          className="text-white font-belanosima-semibold group-hover:text-black transition duration-150 group-active:text-white"
-        >
+          className="text-white font-belanosima-semibold group-hover:text-black transition duration-150 group-active:text-white" >
           Confirm
         </Link>
       </div>
--- src/components/Register/SingIn.tsx
+++ src/components/Register/SingIn.tsx
@@ -1,5 +1,8 @@
 export default function SingIn() {
   return (
-    <div>SingIn</div>
-  )
+    <section className="mt-32  text-white flex flex-col items-center">
+      <h1 className="font-belanosima-semibold text-5xl">Sing Up</h1>
+      <input type="text" className="register-inputs" />
+    </section>
+  );
 }
