94cd2c8a90b1f292de0c801d75deb0bafe18e106
Serio_Mir <voidtracer6@gmail.com>
2023-07-25 14:10:19 -0400

    ./scr/components/ToDo/NewTas/NewTask.tsx: MOD
    initial layout of the component

--- src/components/ToDo/NewTask/NewtTask.tsx
+++ src/components/ToDo/NewTask/NewtTask.tsx
@@ -1,7 +1,27 @@
 export default function NewTask() {
   return (
-    <section className="text-white mt-[60px]">
-      <h1>New Task</h1>
+    <section className="text-white mt-[5rem] ml-[4rem] h-[47rem] rounded-xl w-[50rem] border">
+      <h1 className="mt-[2rem] font-belanosima-semibold  inline-block ml-[43%] text-4xl">
+        New Task
+      </h1>
+      <form
+        action=""
+        className="flex flex-col gap-2 ml-[2rem] mt-4 font-belanosima"
+      >
+        <ul>
+          <li className="flex items-center">
+            <p className="mr-2 text-2xl">Title:</p>
+            <input type="text"
+            className="task-inputs" />
+          </li>
+          <li className="flex items-center mt-4">
+            <p className="mr-2 text-2xl">Title:</p>
+            <input type="text-field"
+            
+            className="task-inputs" />
+          </li>
+        </ul>
+      </form>
     </section>
-  )
+  );
 }
