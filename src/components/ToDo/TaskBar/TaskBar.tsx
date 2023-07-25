7c38a099f73178b7ae1e133c3b9d89e040cdb4b7
Serio_Mir <voidtracer6@gmail.com>
2023-07-25 13:05:51 -0400

    ./scr/components/ToDo/NewTas/NewTask.tsx: ADD
    new component to add new tasks

--- src/components/ToDo/TaskBar/TaskBar.tsx
+++ src/components/ToDo/TaskBar/TaskBar.tsx
@@ -1,10 +1,16 @@
-export default function TaskBar() {
+interface Props {
+  setNewTask: ( newTask : boolean) => void,
+}
+
+export default function TaskBar({ setNewTask }: Props) {
   return (
-  <section className="bg-slate-600 w-[18rem] h-screen flex">
-    <button
-    //onClick={}
-    className="border mx-auto h-[3rem] mt-3 px-4">
-      Add task
-    </button>
-  </section>);
+    <section className="bg-slate-600 w-[18rem] h-screen flex">
+      <button
+        onClick={() => setNewTask(true) }
+        className=" mx-auto h-[3rem] mt-3 px-4 cursor-pointer select-none rounded-lg border-2 active:border-purple-500 active:bg-gray-700 hover:bg-slate-50 text-white font-belanosima-semibold hover:text-black active:text-white  transition duration-150"
+      >
+        + Add task
+      </button>
+    </section>
+  );
 }
--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import TaskBar from "./TaskBar/TaskBar";
+import NewTask from "./NewTask/NewtTask";
 
 export default function ToDo() {
-  return <section className="text-white">
-    <TaskBar />
-  </section>;
+  
+  const [newTask, setNewTask] = useState(false);
+
+  useEffect(() => {
+    console.log(newTask);
+  }, [newTask]);
+
+  return (
+    <section className="text-white">
+      <TaskBar newTask={setNewTask} />
+      {newTask && <NewTask />}
+    </section>
+  );
 }
